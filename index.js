62609e6eac194c6fb1b1298f3c387d139341dd82
𝙱𝚛𝚞𝚡𝚊 𝙻𝚘𝚛𝚍 <shahriarnafisshawon@gmail.com>
2025-12-09 22:36:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+//This is just a test, not main file
 
-const socialmediadl = require('./bruxa');
+const allmediadl = require('./bruxa');
 
 async function testDownload() {
   try {
     // Test 1: Auto-detection with YouTube
     console.log('Test 1: YouTube Auto-detection');
-    const youtubeResult = await socialmediadl.download('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    const youtubeResult = await allmediadl.download('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
     console.log('Platform:', youtubeResult.platform);
     console.log('Data:', JSON.stringify(youtubeResult.data, null, 2));
     console.log('\n---\n');
 
     // Test 2: Direct TikTok function
     console.log('Test 2: Direct TikTok function');
-    const tiktokResult = await socialmediadl.fetchTiktokData('https://www.tiktok.com/@sumaiya_mimu/video/7556912711913803015?is_from_webapp=1&sender_device=pc');
+    const tiktokResult = await allmediadl.fetchTiktokData('https://www.tiktok.com/@sumaiya_mimu/video/7556912711913803015?is_from_webapp=1&sender_device=pc');
     console.log('Data:', JSON.stringify(tiktokResult, null, 2));
     console.log('\n---\n');
 
     // Test 3: Auto-detection with Instagram
     console.log('Test 3: Instagram Auto-detection');
-    const instaResult = await socialmediadl.download('https://www.instagram.com/reel/DPQvNXpjkSb/?igsh=MWRqeWE3eXZjMnBsag==');
+    const instaResult = await allmediadl.download('https://www.instagram.com/reel/DPQvNXpjkSb/?igsh=MWRqeWE3eXZjMnBsag==');
     console.log('Platform:', instaResult.platform);
     console.log('Data:', JSON.stringify(instaResult.data, null, 2));
     
